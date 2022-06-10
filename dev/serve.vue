90dbdf860b4f1e364f1f23bd56314bf5134c3309
jhomin-MN <jhomin-mn@jhomin-MNui-MacBookPro.local>
2022-06-10 17:31:41 +0900

    feat: init sfc-rollup

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueDragAndDropBuilderSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VueDragAndDropBuilderSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-drag-and-drop-builder-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VueDragAndDropBuilderSample } from './vue-drag-and-drop-builder-sample.vue';
